63622919daa705b2e7e5c8f6a1bf8c2c9b38884e
Colesnic Maxim <Maxim.Colesnic@endava.com>
2020-05-21 15:04:03 +0300

    simple twiterClone with autorization

---
+++ src/main/java/com/company/domain/User.java
@@ -0,0 +1,61 @@
+package com.company.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "usr")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String username;
+    String password;
+    boolean active;
+
+    @ElementCollection(targetClass = Role.class,fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role",joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> rols) {
+        this.roles = rols;
+    }
+}
---
+++ src/main/java/com/company/repos/UserRepo.java
@@ -0,0 +1,9 @@
+package com.company.repos;
+
+import com.company.domain.User;
+import org.hibernate.cfg.JPAIndexHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
